307adbe13065ff39c3740da632f893db88aa446e
ishantbhargava <95399064+ishantbhargava@users.noreply.github.com>
2022-02-04 19:29:57 +0530

    banana speak

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("txt-input");
-console.log(txtInput)
-function clickEventhandler() {
-    console.log("clicked!")
-    console.log("input", txtInput,value);
-};
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click", clickthandler)
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after sometime")
+}
+
+function clickHandler() {
+
+    var inputText = txtInput.value;
+       
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+            var translatedText = json.contents.translated; 
+            outputDiv.innerText = translatedText;
+        })
+    .catch(errorHandler)
+
+    };
+btnTranslate.addEventListener("click", clickHandler)
